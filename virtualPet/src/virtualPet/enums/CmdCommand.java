82cfef4836ee6a972029d77e420fe784d13dc64c
ZP <ZP151189@163.com>
2014-04-18 13:47:17 +0800

    zz

---
+++ virtualPet/src/virtualPet/enums/CmdCommand.java
@@ -0,0 +1,42 @@
+package virtualPet.enums;
+
+public enum CmdCommand {
+
+	sndrec32(0,"sndrec32","打开录音机"),
+	Nslookup(1,"Nslookup","IP地址侦测器"),
+	notepad(2,"notepad","打开记事本");
+	
+	private int code;
+	private String command;
+	private String desc;
+	
+	private CmdCommand(int code,String command,String desc){
+		this.code = code;
+		this.command = command;
+		this.desc = desc;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
